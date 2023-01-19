3960c480623883480fc00d79624c268ea2955b3b
nayanjr <kamalnayan.edu2020@gmail.com>
2023-01-18 18:55:44 -0600

    Initial Commit with the project structure

---
+++ src/test/java/com/kamal/RewardsProgram/RewardsProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamal.RewardsProgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardsProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
