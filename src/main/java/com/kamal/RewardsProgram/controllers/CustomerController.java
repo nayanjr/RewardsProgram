d0334342f43d833560083fdc2b0eba9a9b46eb63
nayanjr <kamalnayan.edu2020@gmail.com>
2023-01-20 20:35:36 -0600

    Changes to Logic

--- src/main/java/com/kamal/RewardsProgram/controllers/CustomerController.java
+++ src/main/java/com/kamal/RewardsProgram/controllers/CustomerController.java
@@ -3,18 +3,20 @@ package com.kamal.RewardsProgram.controllers;
 import com.kamal.RewardsProgram.dto.CustomerRewardsDto;
 import com.kamal.RewardsProgram.entities.Customer;
 import com.kamal.RewardsProgram.services.CustomerService;
-import com.kamal.RewardsProgram.comparators.CustomerComparator;
 import com.kamal.RewardsProgram.converters.CustomerConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
+//@CrossOrigin(origins = "http://localhost:3000/")
 @Controller
 @RequestMapping("/customers")
 public class CustomerController {
@@ -24,17 +26,27 @@ public class CustomerController {
     @Autowired
     CustomerService customerService;
 
+
     @GetMapping("/rewards")
     public String displayCustomerRewards(Model model) {
 
-        Comparator comparator = new CustomerComparator();
-
         List<Customer> customers = customerService.getAll();
-        Collections.sort(customers, comparator);
         List<CustomerRewardsDto> customerRewardsDto = customerConverter.entityListToCustomerRewardsDtoList(customers);
         model.addAttribute("customerRewardsList", customerRewardsDto);
 
         return "customers/rewards";
     }
+
+//    @GetMapping("/rewards")
+//    public ResponseEntity<List> displayCustomerRewards() {
+//
+//        List<Customer> customers = customerService.getAll();
+//        List<CustomerRewardsDto> customerRewardsDto = customerConverter.entityListToCustomerRewardsDtoList(customers);
+//
+//        return new ResponseEntity<List>(customerRewardsDto, HttpStatus.OK);
+//
+//
+//    }
+
 
 }
